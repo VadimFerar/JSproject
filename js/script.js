5f5e17e132064e04f43002c591b6ccd1b516de5e
Vadim <vadim.ferar@gmail.com>
2020-08-01 11:28:04 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+//const category = 'toys';
+
+//console.log(`https://someurl.com/${category}/5`);
+
+const user = "Ivan";
+
+alert(`Привет, ${user}`);
+//alert('Hello');
+
+//const result = confirm("Are you here?");
+
+//console.log(result);
+
+//const answer = prompt("Вам есть 18?", "18");
+//console.log(typeof(answer));
+//onst answers = [];
+
+//answers[0] = prompt('как ваше имя?', '');
+//answers[1] = prompt('как ваше фамилия?', '');
+//answers[2] = prompt('сколько вам лет?', '');
+
+//console.log(typeof(answers)); 
+
